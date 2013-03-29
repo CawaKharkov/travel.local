d1f37e4095f7c1b046b8dfd1b32871fe534bf80a
sanstorm <sanstorm86@gmail.com>
2013-03-29 22:28:20 +1100

    Revert "fix layout"
    This reverts commit 7d3f9c853e772776bde88dec750af676d552f0e4.

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -10,22 +10,16 @@
 namespace Application;
 
 use Zend\Mvc\ModuleRouteListener;
-use Zend\EventManager\EventInterface;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\Feature\BootstrapListenerInterface;
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\ModuleManager\Feature\ServiceProviderInterface;
 use Zend\Mvc\MvcEvent;
 
-class Module implements AutoloaderProviderInterface
+class Module
 {
-    public function onBootstrap(EventInterface $e)
+    public function onBootstrap(MvcEvent $e)
     {
         $e->getApplication()->getServiceManager()->get('translator');
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
-        $eventManager->attach('render', array($this, 'initView'));
     }
 
     public function getConfig()
@@ -42,33 +36,5 @@ class Module implements AutoloaderProviderInterface
                 ),
             ),
         );
-    }
-    
-    public function initView(EventInterface $e)
-    {
-        $helperManager = $e->getApplication()->getServiceManager()->get('viewhelpermanager');
-
-        $helperManager->get('headmeta')->setCharset('utf-8')
-                                       ->setName('viewport', 'width=device-width, initial-scale=1.0');
-        
-        $helperManager->get('headtitle')->set('TravelAround '. 'Клуб путешественников')->setSeparator(' - ')->setAutoEscape(false);
-        
-        $helperManager->get('headlink')
-                        ->appendStylesheet('//fonts.googleapis.com/css?family=Open+Sans:400,600,700,800')
-                        ->appendStylesheet('/css/prettyPhoto.css')
-                        ->appendStylesheet('/css/camera.css')
-                        ->appendStylesheet('/css/bootstrap.css')
-                        ->appendStylesheet('/css/theme.css')
-                        ->appendStylesheet('/css/skins/tango/skin.css')
-                        ->appendStylesheet('/css/bootstrap-responsive.css');
-        
-        $helperManager->get('headscript')->appendFile('//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js')
-                                         ->appendFile('/js/jquery.validate.min.js')
-                                         ->appendFile('/js/jquery.form.js')
-                                         ->appendFile('/js/date.js')
-                                         ->appendFile('/js/bootstrap.min.js')
-                                         ->appendFile('/js/bootstrap-datepicker.min.js')
-                                         //->appendFile('//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js')
-                                         ->appendFile('/js/admin.js');
     }
 }
