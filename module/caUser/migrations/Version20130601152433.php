7d823cf911f8d6cd7573e2025a85d1f793300f6e
Cawa <cawa123@mail.ru>
2013-06-01 15:24:49 +0300

    migrations

---
+++ module/caUser/migrations/Version20130601152433.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace caUser\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130601152433 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
